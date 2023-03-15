94996fc2d7075b6b42da6bd616a2e5646cc94b20
Alvar Hønsi <alvarhonsi@gmail.com>
2023-03-15 15:20:53 +0100

    slurm batch sript

--- submit.sh
+++ submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SRUN --job-name=tak011        # create a short name for your job
-#SRUN --nodes=1                # node count
-#SRUN --ntasks=1               # total number of tasks across all nodes
-#SRUN --cpus-per-task=4        # cpu-cores per task (>1 if multi-threaded tasks)
-#SRUN --mem-per-cpu=8G         # memory per cpu-core (4G is default)
-#SRUN --gres=gpu:1             # number of gpus per node
-#SRUN --time=00:20:00          # total run time limit (HH:MM:SS)
-#SRUN --mail-type=begin        # send email when job begins
-#SRUN --mail-type=end          # send email when job ends
-#SRUN --mail-user=tak011@uib.no # email address
-#SRUN --output=slurm-%j.out    # %j is the jobid
-#SRUN --export=ALL
+#SBATCH --job-name=tak011        # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=4        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=8G         # memory per cpu-core (4G is default)
+#SBATCH --gres=gpu:1             # number of gpus per node
+#SBATCH --time=00:20:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-user=tak011@uib.no # email address
+#SBATCH --output=slurm-%j.out    # %j is the jobid
+#SBATCH --export=ALL
 
 conda activate master
 
