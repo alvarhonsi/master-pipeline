5ea15f33db125f304f383b1909ac6d21caabc8fc
tak011 <tak011@birget.uib.no>
2023-03-17 19:35:49 +0100

    model test run

--- submit.sh
+++ submit.sh
@@ -5,19 +5,19 @@
 #SBATCH --cpus-per-task=4        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=8G         # memory per cpu-core (4G is default)
 #SBATCH --gres=gpu:1             # number of gpus per node
-#SBATCH --time=00:20:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=06:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=tak011@uib.no # email address
-#SBATCH --output=slurm-%j.out    # %j is the jobid
+#SBATCH --output=./logs/slurm-%j.out    # %j is the jobid
 #SBATCH --export=ALL
 
 echo "Running on $SLURM_JOB_NODELIST"
 
 # ACTIVATE ANACONDA
 eval "$(conda shell.bash hook)"
-conda activate ai-benchmark
+conda activate master
 
-python run-pipeline.py --dir /experiments/model-test/ --generate --train --eval
+python run-pipeline.py --dir ./experiments/model-test/ --train --eval -p one-layer-sum
 
 echo "Done"
