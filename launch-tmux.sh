cc34dd0c4644aa6568712a2ef69c13b7d6bd850d
Alvar Hønsi <alvarhonsi@gmail.com>
2023-04-11 19:54:18 +0200

    fix

--- launch-tmux.sh
+++ launch-tmux.sh
@@ -8,6 +8,8 @@ do
     echo "starting $SESH"
     echo $line
     tmux new-session -d -s $SESH
-    tmux send-keys -t $SESH "conda run -n master $line" Enter
+    tmux send-keys -t $SESH "conda activate master" Enter
+    sleep 0.5
+    tmux send-keys -t $SESH $line Enter
     COUNT=$((COUNT + 1))
 done < "$INPUT"
