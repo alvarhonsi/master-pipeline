ee44e7bb81ef67ad6c4399c73ded5fe7736c29ff
Alvar Hønsi <alvarhonsi@gmail.com>
2023-04-11 19:17:41 +0200

    tmux run command

---
+++ launch-tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NAME=$2
+COUNT=1
+for CMD in `cat $1`
+do
+    SESH=$NAME$COUNT
+    echo "starting $SESH"
+    echo $CMD
+    tmux new-session -d -s $SESH -n myWindow
+    tmux send-keys -t $SESH:myWindow $CMD Enter
+    COUNT=$((COUNT + 1))
+done
