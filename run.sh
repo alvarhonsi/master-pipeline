55333a61fbd3f8bfa0d0f279c1b07ac7bbd7b6e7
Alvar Hønsi <alvarhonsi@gmail.com>
2023-04-13 11:13:50 +0200

    run sh

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PROPSFILE=$1
+NAME=$2
+COUNT=1
+
+# array declaration
+declare -A props
+
+while IFS='= ' read -r key value; do
+    props["$key"]="$value"
+done < "$PROPSFILE"
+
+DIR=${props["dir"]}
+IFS=', ' read -r -a DEVICES <<< "${props["devices"]}"
+IFS=',' read -r -a PROFILES <<< "${props["profiles"]}"
+
+for i in "${!PROFILES[@]}"
+do
+    DEV=${DEVICES[$i]}
+    PROF=${PROFILES[$i]}
+    CMD="python run-pipeline.py -dir $DIR --train --eval -p $PROF --device $DEV"
+
+    SESH="$NAME-$COUNT"
+    echo "starting $SESH"
+    echo $CMD
+    tmux new-session -d -s $SESH
+    tmux send-keys -t $SESH "conda activate master" Enter
+    sleep 2
+    tmux send-keys -t $SESH $CMD Enter
+    COUNT=$((COUNT + 1))
+done
