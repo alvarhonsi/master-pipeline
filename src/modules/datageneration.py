379e6af9487e0ca6d692598cf74da29f12b828d8
Alvar Hønsi <alvarhonsi@gmail.com>
2023-06-18 10:15:11 +0200

    fix

--- src/modules/datageneration.py
+++ src/modules/datageneration.py
@@ -23,13 +23,14 @@ def tendim_sinusoidal_combination(xs, noise=0) -> np.array:
 
 
 def tendim_sinusoidal_combination2(xs, noise=0) -> np.array:
-    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = xs[:, 0], xs[:, 1],
-    xs[:, 2], xs[:, 3], xs[:, 4], xs[:, 5], xs[:, 6], xs[:, 7], xs[:, 8], xs[:, 9]
+    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = xs[:, 0], xs[:, 1], xs[:,
+                                                                     2], xs[:, 3], xs[:, 4], xs[:, 5], xs[:, 6], xs[:, 7], xs[:, 8], xs[:, 9]
     return 0*x1 + 5 * np.sin(0.2 * x2 * x3) + 5 * np.sin(0.8 * x4) - 5 * np.sin(0.2 * x5 * x6) + 5 * np.sin(0.8 * x7) + 0*x8 + 5 * np.sin(0.2 * x9 * x10) + noise
 
 
 def tendim_non_linear_combination(xs, noise=0) -> np.array:
-    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = xs[:, 0], xs[:, 1], xs[:,2], xs[:, 3], xs[:, 4], xs[:, 5], xs[:, 6], xs[:, 7], xs[:, 8], xs[:, 9]
+    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = xs[:, 0], xs[:, 1], xs[:,
+                                                                     2], xs[:, 3], xs[:, 4], xs[:, 5], xs[:, 6], xs[:, 7], xs[:, 8], xs[:, 9]
     return 2*x1 + np.abs(x1*x2 - 3*x3) + 3*x2 + np.pow((x1*x3)/4, 2) + 0*x4 - 1.2*np.pow(x5, 2) + 2*x6*x7 + 2*x8 + 0*x9 - np.pow(x10/3, 2) + noise
 
 
